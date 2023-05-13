520236866a7a260e3d9d388fc5a286e1c5a97b9e
Fabio Teichmann <fabio.teichmann@icloud.com>
2023-05-13 16:18:28 +0300

    implemented login route

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -15,3 +15,29 @@ server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
 server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
 
 @server.route("/login", methods=["POST"])
+def login():
+    # basic authentication header
+    # contains
+    # auth.username
+    # auth.password
+    auth = request.authorization
+    if not auth:
+        return "missing credentials", 401
+    
+    # check db for username and password
+    cur = MySQL.connection.cursor()
+    res = cur.execute(
+        "SELECT email, password FROM user WHERE email=%s", (auth.username,)
+    )
+    print(res)
+    if res > 0:
+        user_row = cur.fetchone()
+        email = user_row[0]
+        password = user_row[1]
+
+        if auth.username != email or auth.password != password:
+            return "invalid credentials", 401
+        else:
+            return createJWT(auth.username, os.environ["JWT_SECRET"], True)
+    else:
+        return "invalid credentials", 401
